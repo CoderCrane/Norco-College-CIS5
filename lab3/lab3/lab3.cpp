c2c076a5dab984fca7842454967fc7842680bf14
CoderCrane <56849816+CoderCrane@users.noreply.github.com>
2021-01-27 11:09:50 -0800

    Add files via upload

---
+++ lab3/lab3/lab3.cpp
@@ -0,0 +1,18 @@
+#include "pch.h"
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	// Display personal information.
+	cout << "River Crane\n"
+		<< "1234 Long Street\n"
+		<< "Norco, CA 12345\n"
+		<< "(111) 222-3333\n"
+		<< "Computer Programming with Business Application";
+
+
+
+
+	return 0;
+}
